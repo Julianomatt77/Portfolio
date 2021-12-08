0452e6b01d7a7894f4d496585b7b2beb437d483c
Julien MARTIN <ju.martin@outlook.com>
2021-12-08 10:43:35 +0100

    responsive update

--- main.js
+++ main.js
@@ -2,22 +2,27 @@
 
 const experience = document.querySelector("#experienceNav");
 const contact = document.querySelector("#contactNav");
-const NAV = document.querySelector("nav");
+const NAV = document.querySelectorAll("nav li");
 const NAVA = document.querySelectorAll("nav a");
 
 document.addEventListener("DOMContentLoaded", function () {
+  //Change bg color while scrolling
   window.addEventListener("scroll", function () {
     let scrollpos = window.scrollY;
-    if (scrollpos > 400) {
+    if (scrollpos > 250) {
       NAVA.forEach((item) => {
         item.classList.add("darkBlue");
       });
-      NAV.classList.add("bg-orange");
+      NAVA.forEach((item) => {
+        item.classList.add("bg-orange");
+      });
     } else {
       NAVA.forEach((item) => {
         item.classList.remove("darkBlue");
       });
-      NAV.classList.remove("bg-orange");
+      NAVA.forEach((item) => {
+        item.classList.remove("bg-orange");
+      });
       experience.classList.add("darkBlue");
       contact.classList.add("darkBlue");
     }
