ddc1597d318780be28281f5df9dc1a3c635e2169
Julien MARTIN <ju.martin@outlook.com>
2022-07-05 16:52:11 +0200

    responsive + burger color + layout of technologies section

--- js/main.js
+++ js/main.js
@@ -6,37 +6,34 @@ const NAV = document.querySelectorAll("nav");
 const NAVA = document.querySelectorAll("nav a");
 const navLink = document.querySelectorAll(".nav-link");
 const navList = document.querySelector("#navList");
+const burger = document.querySelector("#burgerMenu");
+const burgerIcon = document.querySelector(".navbar-toggler-icon");
+const a =
+	"url(\"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(32, 43, 103, 0.8)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E\") !important;";
 
 document.addEventListener("DOMContentLoaded", function () {
 	//Change bg color while scrolling
 	window.addEventListener("scroll", function () {
 		let scrollpos = window.scrollY;
-		if (scrollpos > 250) {
-			NAVA.forEach((item) => {
-				item.classList.add("darkBlue");
-			});
-			NAV.forEach((item) => {
-				item.classList.add("bg-orange");
-			});
-		} else {
-			NAVA.forEach((item) => {
-				item.classList.remove("darkBlue");
-			});
-			NAV.forEach((item) => {
-				item.classList.remove("bg-orange");
-			});
-			// experience.classList.add("darkBlue");
-			// contact.classList.add("darkBlue");
-		}
-	});
+		let screenWidth = screen.width;
+		if (screenWidth > 767) {
+			if (scrollpos > 250) {
+				NAVA.forEach((item) => {
+					item.classList.add("darkBlue");
+				});
 
-	navList.addEventListener("mouseover", function (event) {
-		event.target.classList.add("darkBlue");
-	});
-	navList.addEventListener("mouseout", function (event) {
-		let scrollpos = window.scrollY;
-		if (scrollpos < 250) {
-			event.target.classList.remove("darkBlue");
+				NAV.forEach((item) => {
+					item.classList.add("bg-orange");
+				});
+			} else {
+				NAVA.forEach((item) => {
+					item.classList.remove("darkBlue");
+				});
+				NAV.forEach((item) => {
+					item.classList.remove("bg-orange");
+				});
+				// burger.classList.remove("navbar-toggler-iconBlue");
+			}
 		}
 	});
 
