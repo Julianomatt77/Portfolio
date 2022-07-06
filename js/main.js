b0db62ae943b119f3a989c96efb177c0f0b61f1e
Julien MARTIN <ju.martin@outlook.com>
2022-07-06 10:26:00 +0200

    modifs on js file, glitch effects and logos

--- js/main.js
+++ js/main.js
@@ -11,30 +11,35 @@ const burgerIcon = document.querySelector(".navbar-toggler-icon");
 const a =
 	"url(\"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(32, 43, 103, 0.8)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E\") !important;";
 
+const events = ["resize", "scroll"];
+
 document.addEventListener("DOMContentLoaded", function () {
 	//Change bg color while scrolling
-	window.addEventListener("scroll", function () {
-		let scrollpos = window.scrollY;
-		let screenWidth = screen.width;
-		if (screenWidth > 767) {
-			if (scrollpos > 250) {
-				NAVA.forEach((item) => {
-					item.classList.add("darkBlue");
-				});
 
-				NAV.forEach((item) => {
-					item.classList.add("bg-orange");
-				});
-			} else {
-				NAVA.forEach((item) => {
-					item.classList.remove("darkBlue");
-				});
-				NAV.forEach((item) => {
-					item.classList.remove("bg-orange");
-				});
-				// burger.classList.remove("navbar-toggler-iconBlue");
+	events.forEach((evt) => {
+		window.addEventListener(evt, function () {
+			let screenWidth = document.documentElement.clientWidth;
+			console.log(screenWidth);
+			let scrollpos = window.scrollY;
+			if (screenWidth > 767) {
+				if (scrollpos > 250) {
+					NAVA.forEach((item) => {
+						item.classList.add("darkBlue");
+					});
+
+					NAV.forEach((item) => {
+						item.classList.add("bg-orange");
+					});
+				} else {
+					NAVA.forEach((item) => {
+						item.classList.remove("darkBlue");
+					});
+					NAV.forEach((item) => {
+						item.classList.remove("bg-orange");
+					});
+				}
 			}
-		}
+		});
 	});
 
 	AOS.init();
